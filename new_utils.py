82694d22fd055b03a5e1323bc31a9cd38aa0eea5
saideepikaneeluru <158000517+saideepikaneeluru@users.noreply.github.com>
2024-02-26 21:23:47 -0500

    Update new_utils.py

--- new_utils.py
+++ new_utils.py
@@ -1,79 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-# new_utils.py
-
 import numpy as np
 from numpy.typing import NDArray
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-ss=StandardScaler()
-def scale_data(alp):
-  scaled_x=alp.astype('float32')/255.0
-  return scaled_x
 
-def scale(alp):
-    if np.max(alp)<=1 and np.min(alp)>=0:
-      return True
+scaler_instance = StandardScaler()
+
+def scale_data(input_array):
+    scaled_values = input_array.astype('float32') / 255.0
+    return scaled_values
+
+def check_scaling(input_array):
+    if np.max(input_array) <= 1 and np.min(input_array) >= 0:
+        return True
     else:
-      return False
-    
-def checklabels(y):
-    val=np.unique(y)
-    for i in val:
-       if type(i)=='str':
-            return 'String Type'  
+        return False
+
+def check_labels(label_array):
+    unique_values = np.unique(label_array)
+    for val in unique_values:
+        if type(val) == 'str':
+            return 'String Type'
     else:
         return 'Integers'
-def accuracy(cm):
-   return np.diagonal(cm).sum() / np.sum(cm)
 
-def remove_90_9s(X: NDArray[np.floating], y: NDArray[np.int32]):
-    """
-    Filter the dataset to include only the digits 7 and 9.
-    Parameters:
-        X: Data matrix
-        y: Labels
-    Returns:
-        Filtered data matrix and labels
-    Notes:
-        np.int32 is a type with a range based on 32-bit ints
-        np.int has no bound; it can hold arbitrarily long numbers
-    """
-  
-    nine_idx = (y == 9)
-    #print('Ids of nine',nine_idx)
-    X_90 = X[nine_idx, :]
-    y_90 = y[nine_idx]
-    #print(int((X_90.shape[0])*0.1))
-    X_90=X_90[:int((X_90.shape[0])*0.1),:]
-    y_90=y_90[:int((y_90.shape[0])*0.1)]
-    none_nine= (y!=9)
-    X_non_9 = X[none_nine, :]
-    y_non_9 = y[none_nine]
-    fin_X=np.concatenate((X_non_9,X_90),axis=0)
-    fin_y=np.concatenate((y_non_9,y_90),axis=0)
-    return fin_X, fin_y
+def calculate_accuracy(confusion_matrix):
+    return np.diagonal(confusion_matrix).sum() / np.sum(confusion_matrix)
 
-def convert_7_0(X: NDArray[np.floating], y: NDArray[np.int32]):
-   id_7=(y==7)
-   id_0=(y==0)
-   y[id_7]=0
-  #  try:
-  #   X[id_7,:]=X[id_0,:]
-  #  except Exception as error:
-  #     print(error)
-   return X,y
+def filter_90_9s(data: NDArray[np.floating], labels: NDArray[np.int32]):
+    nine_indices = (labels == 9)
+    data_90 = data[nine_indices, :]
+    labels_90 = labels[nine_indices]
 
-def convert_9_1(X: NDArray[np.floating], y: NDArray[np.int32]):
-   id_9=(y==9)
-   id_1=(y==1)
-   y[id_9]=1
-  #  try:
-  #   X[id_7,:]=X[id_0,:]
-  #  except Exception as error:
-  #     print(error)
-   return X,y
+    data_90 = data_90[:int((data_90.shape[0]) * 0.1), :]
+    labels_90 = labels_90[:int((labels_90.shape[0]) * 0.1)]
+
+    non_nine_indices = (labels != 9)
+    data_non_9 = data[non_nine_indices, :]
+    labels_non_9 = labels[non_nine_indices]
+
+    final_data = np.concatenate((data_non_9, data_90), axis=0)
+    final_labels = np.concatenate((labels_non_9, labels_90), axis=0)
+
+    return final_data, final_labels
+
+def convert_7_0(data: NDArray[np.floating], labels: NDArray[np.int32]):
+    id_7 = (labels == 7)
+    id_0 = (labels == 0)
+    labels[id_7] = 0
+
+    return data, labels
+
+def convert_9_1(data: NDArray[np.floating], labels: NDArray[np.int32]):
+    id_9 = (labels == 9)
+    id_1 = (labels == 1)
+    labels[id_9] = 1
+
+    return data, labels
+
